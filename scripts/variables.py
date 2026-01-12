c549458edd1d10823e86dd3a465f30b9af457145
Chris Hooper <chris.ryan.hooper@live.co.uk>
2026-01-12 10:08:31 +0000

    Update variables.py - few data types examples added

--- scripts/variables.py
+++ scripts/variables.py
@@ -6,3 +6,19 @@ random = 2358
 
 print(name)
 print(age)
+
+#name = Alice
+#print(name)
+
+age = "25"
+height = 180
+
+print(age + age)
+print(height + height)
+
+result1 = int(age) + height
+result2 = age + str(height)
+
+print(result1)
+print(result2)
+
