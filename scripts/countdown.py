35eb73d61834adcf8faa3da7b8af6f36b5b8339d
Chris Hooper <chris.ryan.hooper@live.co.uk>
2026-01-12 11:00:49 +0000

    Add countdown.py and loops.py - simple loop scripts

---
+++ scripts/countdown.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+#Simple countdown script
+
+for number in range(10,0,-1):
+	print(f"Counting {number}...")
+print("Blast off!")
+
---
+++ scripts/loops.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+#Simple loop script - helping remember for, while and any other loops
+
+fruits = ["apple", "banana", "orange", "mango"]
+for fruit in fruits:
+	print(f"I like {fruit}")
+
